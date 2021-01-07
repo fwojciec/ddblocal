e169b5550af99415b8c2f9062e4e9c0f6a03fb64
Filip Wojciechowski <fwojciec@gmail.com>
2021-01-07 21:05:48 +0100

    consolidate tests to a single file

--- ddblocal_test.go
+++ ddblocal_test.go
@@ -324,6 +324,49 @@ func TestRunnerDeletesTableCorrectly(t *testing.T) {
 	equals(t, expDeleteTableInput, recDeleteTableInput)
 }
 
+func TestEmulatorIntegration(t *testing.T) {
+	if !*integration {
+		t.SkipNow()
+	}
+
+	tableInput := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("Key"),
+				AttributeType: aws.String("S"),
+			},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("Key"),
+				KeyType:       aws.String("HASH"),
+			},
+		},
+	}
+
+	ddb.Runner(t, tableInput, func(client dynamodbiface.DynamoDBAPI, tableName string) {
+		_, err := client.PutItem(&dynamodb.PutItemInput{
+			TableName: aws.String(tableName),
+			Item: map[string]*dynamodb.AttributeValue{
+				"Key": {S: aws.String("Hello")},
+			},
+		})
+		ok(t, err)
+
+		res, err := client.GetItem(&dynamodb.GetItemInput{
+			TableName: aws.String(tableName),
+			Key: map[string]*dynamodb.AttributeValue{
+				"Key": {S: aws.String("Hello")},
+			},
+		})
+		ok(t, err)
+
+		key := res.Item["Key"].S
+		assert(t, key != nil, "key should not be nil")
+		equals(t, "Hello", *key)
+	})
+}
+
 // ok fails the test if an err is not nil.
 func ok(tb testing.TB, err error) {
 	if err != nil {
